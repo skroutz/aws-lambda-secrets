47602d6b1147e86e5674d0786e90507b3017add6
John Torakis <jtorakis@skroutz.gr>
2022-07-27 15:51:01 +0300

    tests: working e2e Lambda

--- tests/terraform/secretsmanager.tf
+++ tests/terraform/secretsmanager.tf
@@ -1,55 +1,46 @@
 resource "aws_secretsmanager_secret" "plain" {
-  name = "${local.secretsmanager-path}/simple"
+  name = "${local.secretsmanager-path}/plain"
+  description = "Automated test resource for 'lambda-secrets'"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "plain" {
   secret_id     = aws_secretsmanager_secret.plain.id
-  secret_string = "S3CR37 L1N3"
+  secret_string = var.secret-plain
 }
 
 
 resource "aws_secretsmanager_secret" "multiline" {
   name = "${local.secretsmanager-path}/multiline"
+  description = "Automated test resource for 'lambda-secrets'"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "multiline" {
   secret_id     = aws_secretsmanager_secret.multiline.id
-  secret_string = <<EOF
-This is
-a
-Multiline
-Secr3t!
-EOF
+  secret_string = var.secret-multiline
 }
 
 
 resource "aws_secretsmanager_secret" "json" {
   name = "${local.secretsmanager-path}/json"
+  description = "Automated test resource for 'lambda-secrets'"
+  recovery_window_in_days = 0
 }
-
 
 resource "aws_secretsmanager_secret_version" "json" {
   secret_id     = aws_secretsmanager_secret.json.id
-  secret_string = <<EOF
-{
-  "username": "admin",
-  "password": "p@55w0rd!"
+  secret_string = var.secret-json
 }
-EOF
-}
+
 
 resource "aws_secretsmanager_secret" "binary" {
   name = "${local.secretsmanager-path}/binary"
+  description = "Automated test resource for 'lambda-secrets'"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "binary" {
   secret_id     = aws_secretsmanager_secret.binary.id
-  secret_binary = base64encode(<<EOF
-{
-  "username": "admin",
-  "password": "p@55w0rd!"
-  "binary": true
-}
-EOF
-)
+  secret_binary = var.secret-binary
 }
