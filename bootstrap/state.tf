861490525d3c68d1e29f35bb4e3ce47527d533f8
Giannis Christinakis <50832176+gian2dchris@users.noreply.github.com>
2022-08-24 15:19:19 +0300

    bootstrap: state fixup

--- bootstrap/state.tf
+++ bootstrap/state.tf
@@ -5,9 +5,9 @@ terraform {
 
     # Values as generated from 'bootstrap/'
     region         = "eu-central-1"
-    bucket         = "lambda-secrets-app-state-533973265978"
-    dynamodb_table = "lambda-secrets-app-state"
-    kms_key_id     = "b846806b-b83f-4ac5-b3b0-92505b31bdeb"
+    bucket         = "aws-lambda-secrets-layer-state-533973265978"
+    dynamodb_table = "aws-lambda-secrets-layer-state"
+    kms_key_id     = "e740f26a-7474-4774-9302-583f98aed23f"
 
     key            = "tf-state/aws-lambda-secrets/terraform.tfstate"
   }
